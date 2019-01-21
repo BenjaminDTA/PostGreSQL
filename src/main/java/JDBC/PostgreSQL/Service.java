a8efecfeb0056e82a01e5aade2fecc3327a4a0de
benjamin <benjam.montet@gmail.com>
2019-01-21 16:00:58 +0100

    5465

--- src/main/java/JDBC/PostgreSQL/Service.java
+++ src/main/java/JDBC/PostgreSQL/Service.java
@@ -19,27 +19,29 @@ public class Service {
 	public static void addBook(Connection conn, Book bk) throws SQLException {
 
 		PreparedStatement insert1 = null;
+		ResultSet generatedKeys = null;
 		try {
 			insert1 = conn.prepareStatement("INSERT INTO book(title, author) VALUES(?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
 			insert1.setString(1, bk.getTitle());
 			insert1.setString(2, bk.getAuthor());
 			insert1.executeUpdate();
-			ResultSet generatedKeys = insert1.getGeneratedKeys();
+			generatedKeys = insert1.getGeneratedKeys();
 			generatedKeys.next();
 			bk.setId(generatedKeys.getInt("id"));
-			insert1.close();
 
 		} catch (SQLException e) {
 			logger.info("",e);
 		}finally {
 			insert1.close();
+			generatedKeys.close();
 		}
 
 	}
 
 	public static void addClient(Connection conn, Client c) throws SQLException {
 		PreparedStatement insert2 = null;
+		ResultSet generatedKeys = null;
 		try {
 			insert2 = conn.prepareStatement(
 					"INSERT INTO client(lastname, firstname, gender, id_book) VALUES (?, ?, ?, ?)",
@@ -49,7 +51,7 @@ public class Service {
 			insert2.setString(3, c.getGender());
 			insert2.setInt(4, c.getidLivrePref());
 			insert2.executeUpdate();
-			ResultSet generatedKeys = insert2.getGeneratedKeys();
+			generatedKeys = insert2.getGeneratedKeys();
 			generatedKeys.next();
 			c.setId(generatedKeys.getInt("id"));
 			
@@ -57,6 +59,7 @@ public class Service {
 			logger.info("",e);
 		}finally {
 			insert2.close();
+			generatedKeys.close();
 		}
 
 	}
@@ -79,12 +82,13 @@ public class Service {
 
 	public static void achatClients(Connection conn, Client c) throws SQLException {
 		PreparedStatement insert4 = null;
+		ResultSet resultSet = null;
 
 		try {
 			insert4 = conn.prepareStatement(
 					"Select title, author from book join achat on book.id = achat.id_book join client on achat.id_client = client.id where client.id ="
 							+ c.getId());
-			ResultSet resultSet = insert4.executeQuery();
+			resultSet = insert4.executeQuery();
 			logger.info(c.getFirstname());
 			while (resultSet.next()) {
 			logger.info(resultSet.getString("title") + " " + resultSet.getString("author"));
@@ -95,26 +99,27 @@ public class Service {
 			logger.info("",e);
 		}finally {
 			insert4.close();
+			resultSet.close();
 		}
 	}
 
 	public static void acheteur(Connection conn, Client c) throws SQLException {
 		PreparedStatement insert5 = null;
-
+		ResultSet resultSet = null;
 		try {
 			insert5 = conn.prepareStatement(
 					"Select distinct firstname, lastname from client join achat on client.id = achat.id_book");
-			ResultSet resultSet = insert5.executeQuery();
+			resultSet = insert5.executeQuery();
 			logger.info(c.getFirstname());
 			while (resultSet.next()) {
 				logger.info(resultSet.getString("firstname") + " " + resultSet.getString("lastname"));
 
 			}
-			insert5.close();
 		} catch (SQLException e) {
 			logger.info("",e);
 		}finally {
 			insert5.close();
+			resultSet.close();
 		}
 	}
 }
